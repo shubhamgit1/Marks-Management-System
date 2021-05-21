1521c6359f4f8be452ea712ea343b78d289e2b2f
shubhamgit1 <shubham22019@gmail.com>
2021-05-22 00:02:32 +0530

    created database

--- database.js
+++ database.js
@@ -1,13 +1,45 @@
 const mysql = require('mysql');
-
-const mysqlConnection = mysql.createPool({
-    connectionLimit: 2,
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'sc123456',
-    database: 'almabetter',
-    insecureAuth : true
     
 });
+
+connection.connect(function(err){  
+    if (err) throw err;  
+    console.log("Connected!");  
+    connection.query("CREATE DATABASE IF NOT EXISTS mydb4", function (err, result) {  
+    if (err) throw err;  
+    else{
+        console.log("Database created"); 
+    }
+     
+    });  
+});
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sc123456',
+    database: 'mydb4',
+    
+});
+
+mysqlConnection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE IF NOT EXISTS students (roll_no int not null,\
+                                                    name varchar(50) not null,\
+                                                    maths_marks int not null,\
+                                                    physics_marks int not null,\
+                                                    chemistry_marks int not null,\
+                                                    total_marks int not null,\
+                                                    percentage int not null)";
+    mysqlConnection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+    });
+});
+
 
 module.exports = mysqlConnection;
